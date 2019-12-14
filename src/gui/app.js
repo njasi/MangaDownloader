95ffbb7c10c656fa62d4fd19064fea3f858dc54c
njasi <nick@jasinski3.com>
2019-12-14 16:50:25 -0600

    simple react building for test development stuff thingys

--- src/gui/app.js
+++ src/gui/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+console.log("hello there")
 export default () => {
   return <h1>Hello there</h1>
 }
--- src/gui/index.js
+++ src/gui/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
+import App from './app'
+import history from './history'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
+  // <Provider store={store}>
+  <Router history={history}>
+    <App />
+  </Router>,
+  // </Provider>,
   document.getElementById('app')
 )
